62180811bdc16f9708bdf8d7b49d7eceba3eb49d
Ah9797 <89481488+Ah9797@users.noreply.github.com>
2023-03-02 10:09:26 -0600

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,9 @@
-function Next_phase() {
-  document.getElementById("des").innerHTML = 'Create Username and Password';
-}
+function init(){
 
+function Next_phase() {
+  document.getElementById('des').innerHTML = 'Create Username and Password';
+}
+var b = document.getElementById('button-slot');
+b.addEventListener('click', Next_phase);
+}
+window.addEventListener('load', init);
